dc7d9c5c87025e95c30be518323b567d2a9de282
Nurkholis Majid <noerkholis.madjid@gmail.com>
2026-01-26 20:54:25 +0700

    🔥 refactor(frontend): remove unnecessary files

--- apps/frontend/src/contexts/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthContext } from './AuthContext';
-export type { AuthContextType } from './AuthContext';
-export { AuthProvider } from './AuthProvider';
-export { useAuth } from './useAuth';
--- apps/frontend/src/contexts/useAuth.ts
+++
@@ -1,9 +0,0 @@
-// src/contexts/useAuth.ts
-import { useContext } from 'react';
-import { AuthContext } from './AuthContext';
-
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) throw new Error('useAuth harus di dalam AuthProvider');
-  return context;
-};
