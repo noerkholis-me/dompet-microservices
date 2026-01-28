21e938ae7f9676006b7a7f0fcc3dc324fe643b05
Nurkholis Majid <noerkholis.madjid@gmail.com>
2026-01-28 16:13:22 +0700

    ✨ feat(dto): add api property to loginDto and addToCartDto

--- libs/contracts/src/dto/auth/login.dto.ts
+++ libs/contracts/src/dto/auth/login.dto.ts
@@ -1,4 +1,11 @@
 import { LoginSchema } from '@contracts/schemas/auth';
 import { createZodDto } from 'nestjs-zod';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class LoginDto extends createZodDto(LoginSchema) {}
+export class LoginDto extends createZodDto(LoginSchema) {
+  @ApiProperty({ example: 'admin@dompet.com', description: 'Email' })
+  email: string;
+
+  @ApiProperty({ example: 'admin123', description: 'Password' })
+  password: string;
+}
--- libs/contracts/src/dto/transaction/add-to-cart.dto.ts
+++ libs/contracts/src/dto/transaction/add-to-cart.dto.ts
@@ -1,4 +1,11 @@
 import { createZodDto } from 'nestjs-zod';
 import { AddToCartSchema } from '@contracts/schemas/transaction';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class AddToCartDto extends createZodDto(AddToCartSchema) {}
+export class AddToCartDto extends createZodDto(AddToCartSchema) {
+  @ApiProperty({ example: 'masukan product ID', description: 'Product ID' })
+  productId: string;
+
+  @ApiProperty({ example: 5, description: 'Quantity' })
+  quantity: number;
+}
