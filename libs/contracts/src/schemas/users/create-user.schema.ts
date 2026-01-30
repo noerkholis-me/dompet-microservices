dd2ce45696eb2b2b6abe8760c946bb874a6f0207
Nurkholis Majid <noerkholis.madjid@gmail.com>
2026-01-30 09:54:49 +0700

    ✨ feat(frontend): adjust schema create and update user

--- libs/contracts/src/schemas/users/create-user.schema.ts
+++ libs/contracts/src/schemas/users/create-user.schema.ts
@@ -2,10 +2,10 @@ import { RoleType } from '@contracts/enums/role.enum';
 import { z } from 'zod';
 
 export const CreateUserSchema = z.object({
-  name: z.string(),
-  email: z.email(),
+  name: z.string().min(1),
+  email: z.string(),
   password: z.string().min(6),
-  role: z.nativeEnum(RoleType).optional(),
+  role: z.nativeEnum(RoleType),
   status: z.boolean().optional(),
 });
 
